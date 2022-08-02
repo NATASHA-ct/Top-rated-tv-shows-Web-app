55999f6506e3e0ff348b0054e146580b2af23691
Cindy Dorantes <cindym.dorantes@gmail.com>
2022-08-02 17:55:38 -0500

    add style to show items

--- src/modules/display-home.js
+++ src/modules/display-home.js
@@ -5,21 +5,25 @@ const baseURL = 'https://api.tvmaze.com/lookup/shows?imdb=';
 
 const displayHome = (item) => {
   const showItem = document.createElement('div');
+  showItem.classList.add('showItem');
   displayItems.appendChild(showItem);
 
   const showImg = document.createElement('img');
   showItem.appendChild(showImg);
 
   const showInfo = document.createElement('div');
+  showInfo.classList.add('showInfo');
   showItem.appendChild(showInfo);
 
   const showName = document.createElement('p');
   showInfo.appendChild(showName);
 
   const showLikes = document.createElement('div');
+  showLikes.classList.add('showLikes');
   showInfo.appendChild(showLikes);
 
   const likesIcon = document.createElement('button');
+  likesIcon.classList.add('btnLike');
   likesIcon.innerHTML = '<i class="fa-regular fa-heart"></i>';
   showLikes.appendChild(likesIcon);
 
