3dcca59a7bd0bc5f562ac00fa2b2d5fe41e178c2
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-08-06 01:18:23 +0300

    Add test for comments counter

---
+++ src/MODS/test/comCounter.test.js
@@ -0,0 +1,25 @@
+// MOCKING THE TEST
+
+const getComments = async () => Promise.resolve([
+  {
+    username: 'Harry',
+    comment: 'Best show ever',
+    creation_date: '2022-08-05',
+  },
+]);
+
+// checking length of viewercomments is equal to number of comments given like above we have one.
+const getNumberComments = async () => {
+  let viewerCommentsNum = await getComments();
+  viewerCommentsNum = viewerCommentsNum.length;
+  return viewerCommentsNum;
+};
+
+// if the number of viewer comments is as expected then it passes
+jest.mock('../get-comments.js');
+
+test('The number of comments of the item 0 is 1', () => {
+  getNumberComments().then((comments) => {
+    expect(comments).toBe(1);
+  });
+});
