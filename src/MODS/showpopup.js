495acf2dce04733f145e9407ec9452f9027b4819
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-08-05 17:50:28 +0300

    Uncomment code

--- src/MODS/showpopup.js
+++ src/MODS/showpopup.js
@@ -68,70 +68,70 @@ const showPopup = async (id) => {
 
   commentsPopup.appendChild(showstats);
 
-  // people comments .......
+  //people comments .......
 
-  //    //comments heading
-  //     const commenthd = document.createElement("h2");
-  //     commenthd.className = "hcomments";
-  //     commenthd.innerText = "Viewer's comments";
-  //     commentsPopup.appendChild(commenthd);
+     //comments heading
+      const commenthd = document.createElement("h2");
+      commenthd.className = "hcomments";
+      commenthd.innerText = "Viewer's comments";
+      commentsPopup.appendChild(commenthd);
 
-  //  // given comments
-  //     const viewers = document.createElement("ul");
-  //     viewers.className = "people-comments";
+   // given comments
+      const viewers = document.createElement("ul");
+      viewers.className = "people-comments";
 
-  // //temporary viewer list
-  //     const viewerA= document.createElement("li");
-  //     viewerA.className = "stats";
-  //     viewers.appendChild(viewerA);
+  //temporary viewer list
+      const viewerA= document.createElement("li");
+      viewerA.className = "stats";
+      viewers.appendChild(viewerA);
 
-  //     const viewerB= document.createElement("li");
-  //     viewerB.className = "viewer";
-  //     viewers.appendChild(viewerB);
+      const viewerB= document.createElement("li");
+      viewerB.className = "viewer";
+      viewers.appendChild(viewerB);
 
-  //     commentsPopup.appendChild(viewers);
+      commentsPopup.appendChild(viewers);
 
-  // //FORM ADD VIEWER AND COMMENT
-  //     //main container
-  //         const formsec = document.createElement("section");
-  //         formsec.className = "addComments";
-  //         commentsPopup.appendChild(formsec);
+  //FORM ADD VIEWER AND COMMENT
+      //main container
+          const formsec = document.createElement("section");
+          formsec.className = "addComments";
+          commentsPopup.appendChild(formsec);
 
-  //     //heading of the section
-  //         const hAdd = document.createElement("h2");
-  //         hAdd.className = "hAdd";
-  //         hAdd.innerText = "Add a comment";
-  //         formsec.appendChild(hAdd);
+      //heading of the section
+          const hAdd = document.createElement("h2");
+          hAdd.className = "hAdd";
+          hAdd.innerText = "Add a comment";
+          formsec.appendChild(hAdd);
 
-  //     //FORM .......................
-  //         const formmain = document.createElement("form");
-  //         formmain.action = "#" ;
-  //         formmain.method = "GET" ;
-  //         formsec.appendChild(formmain);
+      //FORM .......................
+          const formmain = document.createElement("form");
+          formmain.action = "#" ;
+          formmain.method = "GET" ;
+          formsec.appendChild(formmain);
 
-  // //inside form inputs
-  // const user = document.createElement("input");
-  // user.className = "text";
-  // user.type = "text" ;
-  // user.id = "username";
-  // user.required = true;
-  // user.placeholder = "Enter your name";
-  // formmain.appendChild(user);
+  //inside form inputs
+  const user = document.createElement("input");
+  user.className = "text";
+  user.type = "text" ;
+  user.id = "username";
+  user.required = true;
+  user.placeholder = "Enter your name";
+  formmain.appendChild(user);
 
-  // const usercomm = document.createElement("input");
-  // usercomm.className = "text";
-  // usercomm.type = "text" ;
-  // usercomm.id = "comment";
-  // usercomm.required = true;
-  // usercomm.placeholder = "Your insights";
-  // formmain.appendChild(usercomm);
+  const usercomm = document.createElement("input");
+  usercomm.className = "text";
+  usercomm.type = "text" ;
+  usercomm.id = "comment";
+  usercomm.required = true;
+  usercomm.placeholder = "Your insights";
+  formmain.appendChild(usercomm);
 
-  // const commBtn = document.createElement("input");
-  // commBtn.className = "com-btn";
-  // commBtn.type = "submit" ;
-  // commBtn.id = "submit";
-  // commBtn.value = "Comment";
-  //  formmain.appendChild(usercomm);
+  const commBtn = document.createElement("input");
+  commBtn.className = "com-btn";
+  commBtn.type = "submit" ;
+  commBtn.id = "submit";
+  commBtn.value = "Comment";
+   formmain.appendChild(usercomm);
 
   body.appendChild(commentsPopup);
 };
